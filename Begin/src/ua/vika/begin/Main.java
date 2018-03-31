df49bc0a2d25dba82762b3237de8c7086eb81f21
user <user@userpc>
2018-03-31 21:29:02 +0300

    add 1 !

--- Begin/src/ua/vika/begin/Main.java
+++ Begin/src/ua/vika/begin/Main.java
@@ -3,7 +3,7 @@ package ua.vika.begin;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Githab");
+		System.out.println("Hello from Githab !");
 	}
 
 }
